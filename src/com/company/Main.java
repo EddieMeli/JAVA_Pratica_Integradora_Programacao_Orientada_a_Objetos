09252dd70841f6a779e393b4a96e5ddac5646f9f
EddieMeli <edenilson.paschoa@mercadolivre.com>
2021-08-27 07:32:42 -0300

    Final

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,16 +1,49 @@
 package com.company;
 
+import java.util.*;
+
 public class Main {
 
         public static void main(String[] args) {
-        // Objeto tipo pessoa
+            // Objeto tipo pessoa
             Pessoa pessoa0 = new Pessoa();
             Pessoa pessoa1 = new Pessoa("Edenilson Teixeira Paschoa", 33, "3");
-            Pessoa pessoa2 = new Pessoa("Edenilson Teixeira Paschoa", 33, "1",85.00);
+            Pessoa pessoa2 = new Pessoa("Edenilson Teixeira Paschoa", 33, "4", 85, 1.71);
+            // -- Pessoa pessoa = new Pessoa("Edenilson Teixeira Paschoa", 33); // Declaração incorreta,
 
-            // -- Pessoa pessoa3 = new Pessoa("Edenilson Teixeira Paschoa", 33); // Declaração incorreta, falta parâmetros
+            String MaiorDeIdade =  "";
+
+            if (pessoa0.eMaiorDeIdade()){
+                MaiorDeIdade = "Sim";
+            }else {
+                MaiorDeIdade = "Não";
+            }
+            System.out.println("Pesspa0 " + MaiorDeIdade + " é maior de idade:");
+            System.out.println("Calculo do IMC Pesspa0:" + pessoa0.calculaIMC());
 
 
 
+
+            if (pessoa1.eMaiorDeIdade()){
+                MaiorDeIdade = "Sim";
+            }else {
+                MaiorDeIdade = "Não";
+            }
+
+            System.out.println("Pesspa1 " + MaiorDeIdade + " é maior de idade:");
+
+            System.out.println("Calculo do IMC Pesspa1:" + pessoa0.calculaIMC());
+
+
+            if (pessoa2.eMaiorDeIdade()){
+                MaiorDeIdade = "Sim";
+            }else {
+                MaiorDeIdade = "Não";
+            }
+
+            System.out.println("Pesspa2 " + MaiorDeIdade + " é maior de idade:");
+
+            System.out.println("Calculo do IMC Pesspa2:" + pessoa2.calculaIMC());
+            // System.out.println(flgMaiorDeIdade);
         }
 }
--- src/com/company/Pessoa.java
+++ src/com/company/Pessoa.java
@@ -1,12 +1,13 @@
 package com.company;
 
 public class Pessoa {
+    double vAltura = 0;
     String vNome;
-    int vIdade;
-    String Id;
-    Double Peso;
+    int vIdade = 0;
+    String Id = "0";
+    double vPeso = 0;
 
-
+    // -- Construtores
     public Pessoa() {
     }
 
@@ -16,10 +17,36 @@ public class Pessoa {
         Id = id;
     }
 
-    public Pessoa(String vNome, int vIdade, String id, Double peso) {
+    public Pessoa(String vNome, int vIdade, String id, double vPeso, double vAltura) {
         this.vNome = vNome;
         this.vIdade = vIdade;
-        Id = id;
-        Peso = peso;
+        this.vAltura = vAltura;
+        this.Id = id;
+        this.vPeso = vPeso;
+    }
+    // -- Fim construtores
+    // -- Metodos
+    public int calculaIMC() {
+        double IMC = 0;
+
+        if (vPeso != 0 && vAltura != 0) {
+            IMC = vPeso / Math.pow(vAltura, 2);
+        }
+        if (IMC < 20) {
+            return 20;
+        } else if (IMC >= 20 && IMC <= 20) {
+            return -1;
+        } else if (IMC >= 25) {
+            return 1;
+        }
+        return 0;
+    }
+
+    public boolean eMaiorDeIdade() {
+        if (vIdade >= 18){
+            return true;
+        } else {
+            return  false;
+        }
     }
 }
